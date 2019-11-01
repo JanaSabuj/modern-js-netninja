0c15a15590422305f9067fd05e98293d2a1eedeb
Sabuj <sabujlovescoding@gmail.com>
2019-11-01 10:17:53 +0530

    Variables

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,30 @@
+console.log('Hello'); 
+
+// variables
+let age = 205;
+let year = 2000;
+console.log(age, year);
+
+age = 300;
+console.log(age);
+
+const x = 10;
+// x = 5; not possible to change values of const variable 
+console.log(x);
+
+var y = 20;
+y = 21;
+console.log(y);
+
+/* Data Types
+
+Number - 1, 220202, 222.545456
+String - 'hello', "world is ending"
+Boolean - true/false
+null - no value - we explicitly set it
+Undefined - implicitly given by the browser to a variablenot yet defined
+Object - UwU
+Symbol - Used with objects
+
+*/
+
