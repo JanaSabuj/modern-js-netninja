2a2d0a3709c674389ac224d318290641a51364e2
Sabuj <sabujlovescoding@gmail.com>
2019-11-02 12:41:24 +0530

    Func with params and args

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -5,7 +5,6 @@ console.log('Sandbox.js is connected');
 function greet(){
     console.log(' Good Morning friends !!!; Star this repository');
 }
-
 greet();
 greet();
 
@@ -13,19 +12,26 @@ greet();
 const speak = function(){
     console.log('Good Afternoon!');
 };
-
 speak();
 speak();
 
-// HOISTING in JS -> All func DECLARATIONS (not expressions not initilaiziations) are automatically
-// hosted to the top of the js file 
+/* HOISTING in JS -> All func DECLARATIONS (not expressions not initilaiziations) are automatically
+   hosted to the top of the js file 
+   eg - var x
+   eg - function greet()
 
-// eg - var x
-// eg - function greet()
+   all these can be written at the end of the .js file and code would still run OK bcoz of hoisting
+   function greet(){
+           console.log(' Good Morning friends !!!; Star this repository');
+    }
+*/
 
-// all these can be written at the end of the .js file and code would still run OK bcoz of hoisting
-// function greet(){
-//         console.log(' Good Morning friends !!!; Star this repository');
-//     }
+// ---------------argument -> in fn call   parameter -> in fn declaration
 
+const wish = function(time = 'night', name = 'sabuj'){
+    console.log(`Good ${time} ${name}`);
+};
+
+wish();
+wish('morn', 'agneyee');
 
