ed4a9793d91055cb9b26bae7cfb710fb7270d00b
Sabuj <sabujlovescoding@gmail.com>
2019-11-02 11:47:40 +0530

    Contol flow end

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -23,9 +23,51 @@ if(ar.length > 2){
     console.log('Worst Array');
 }
 
-// -----------logical AND(&&) OR(||)
+// -----------logical AND(&&) OR(||) NOT(!)
 
 if(ar.length > 2 && ar.includes('mario')){
     console.log('Hello Mario with Good Array');
 }
 
+let user = false;
+
+//  if user is false, then run the code
+
+if(!user){
+    console.log('User must be logged in to continue');
+};
+
+// break and continue
+
+// switch
+let grade = 'D';
+
+// the case is marked by strict inequality ===
+switch(grade){
+    case 'A':
+        console.log('You got an A!'); break;
+    
+    case 'B':
+        console.log('You got an B!');break;
+
+    case 'C':
+        console.log('You got an C!');break;
+
+    case 'D':
+        console.log('You got an D!');break;
+
+    default:
+       console.log('You failed!');
+}
+
+// ---- note block vs global scope .
+
+let age = 20;
+
+if(true){
+    let age = 52;
+    console.log('inside local', age);
+    var name = 'hannah';// var has this disadv. 
+}
+
+console.log('global', age, name);// we can access even locally scoped var globally :(
