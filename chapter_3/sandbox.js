306d8c3ee3b670cfc0dedd1619a9944db999520b
Sabuj <sabujlovescoding@gmail.com>
2019-11-02 10:40:23 +0530

    For Loops

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,1 +1,14 @@
 console.log('This is chapter 3 OF Control Flow!!');
+
+for(let i = 0; i < 5; i++){
+    console.log('in loop', i);
+}
+
+const ar = ['hello', 'shaun', 'mario'];
+for(let i = 0; i < ar.length; i++){
+    console.log(ar[i]);
+    let html = `
+        <div> ${ar[i]} </div>
+    `;
+    console.log(html);
+}
