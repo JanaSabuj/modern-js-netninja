58f20533ece21e045ed62c1bc0f6c05da11069de
Sabuj <sabujlovescoding@gmail.com>
2019-11-03 11:54:06 +0530

    ch 6 end

--- chapter_6/clickSandbox.js
+++ chapter_6/clickSandbox.js
@@ -59,3 +59,9 @@ box.addEventListener('mousemove', (e) => {
     console.log(e.offsetX, e.offsetY);
     box.textContent = `x pos - ${e.offsetX}, y pos - ${e.offsetY}`;
 });
+
+//wheel event
+document.addEventListener('wheel', (e) => {
+    console.log(e);
+    console.log(e.pageX, e.pageY);
+});
